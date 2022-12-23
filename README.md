ad1cb509dea702a1a899331b08b74944d8018859
ergrelet <ergrelet@users.noreply.github.com>
2022-12-23 23:30:29 +0100

    Add an option to enable support for Windows 11
    Bump project to version 0.1.1
    Update README

--- README.md
+++ README.md
@@ -2,17 +2,22 @@
 
 Standalone version of `forklib` from [winnie](https://github.com/sslab-gatech/winnie).
 This version retrieves required `ntdll` symbols at run time and should work on
-all Windows 10 versions.
+all Windows 10 and Windows 11 versions.
+
+Keep in mind that this library is mainly aimed at providing a fork API for
+fuzzing. Many Windows APIs (*e.g.*, `user32` APIs) won't work seemlessly in forked processes.
 
 ## How to Build
 
 ```
-$ cmake -B x64
-$ cmake --build x64 --config Release -- -maxcpucount
+# Use `cmake -B x64 -DFORKLIB_WINDOWS_11=ON` to build for Windows 11
+cmake -B x64
+cmake --build x64 --config Release -- -maxcpucount
 ```
 Note: You may want to set the `FORKLIB_NOTIFY_CSRSS_FROM_PARENT` and
 `FORKLIB_RESTORE_STDIO` CMake options to `OFF` if you plan to build the library
-for `winnie`.
+for `winnie`, as it reduce the overhead of the forking at the cost of reducing
+available Win32 functionalities in the child.
 
 ## How to Use
 
--- src/fork.cpp
+++ src/fork.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <optional>
+#include <string>
 
 // Uncodumented headers csrss stuff
 #include "csrss.h"
@@ -119,7 +120,7 @@ BOOL NotifyCsrssParent(HANDLE hProcess, HANDLE hThread) {
 // API and structures are much simpler on Windows 10 than Windows 7, and as a
 // result our job is much easier.
 BOOL ConnectCsrChild(const CsrRegion& csr_region) {
-  BOOL bIsWow64;
+  BOOL bIsWow64{};
   if (!IsWow64Process(GetCurrentProcess(), &bIsWow64)) {
     LOG("FORKLIB: IsWow64Process failed!\n");
     return FALSE;
@@ -144,34 +145,41 @@ BOOL ConnectCsrChild(const CsrRegion& csr_region) {
   }
 #endif
 
-  DWORD session_id;
-  wchar_t ObjectDirectory[100];
+  DWORD session_id{};
   ProcessIdToSessionId(GetProcessId(GetCurrentProcess()), &session_id);
-  swprintf(ObjectDirectory, 100, L"\\Sessions\\%d\\Windows", session_id);
+
+  wchar_t ObjectDirectory[100]{};
+  swprintf(ObjectDirectory, ARRAYSIZE(ObjectDirectory),
+           L"\\Sessions\\%d\\Windows", session_id);
   LOG("FORKLIB: Session_id: %d\n", session_id);
 
   // Not required?
   LOG("FORKLIB: Link Console subsystem...\n");
   void* pCtrlRoutine =
       (void*)GetProcAddress(GetModuleHandleA("kernelbase"), "CtrlRoutine");
-  BOOLEAN trash;
+  BOOLEAN ServerToServerCall;
   // if (!NT_SUCCESS(CsrClientConnectToServer(L"\\Sessions\\" CSRSS_SESSIONID
-  // L"\\Windows", 1, &pCtrlRoutine, 8, &trash)))
-  if (!NT_SUCCESS(CsrClientConnectToServer(ObjectDirectory, 1, &pCtrlRoutine, 8,
-                                           &trash))) {
+  // L"\\Windows", 1, &pCtrlRoutine, 8, &ServerToServerCall)))
+  if (!NT_SUCCESS(CsrClientConnectToServer(ObjectDirectory, 1, &pCtrlRoutine,
+                                           sizeof(void*),
+                                           &ServerToServerCall))) {
     LOG("FORKLIB: CsrClientConnectToServer failed!\n");
     return FALSE;
   }
 
   LOG("FORKLIB: Link Windows subsystem...\n");
-  // passing &gfServerProcess is not necessary, actually? passing &trash is
-  // okay?
-  char buf[0x240];  // this seem to just be all zero everytime?
+// passing &gfServerProcess is not necessary, actually? passing
+// &ServerToServerCall is okay?
+#ifdef FORKLIB_WINDOWS_11
+  char buf[0x248];  // this seem to just be all zero everytime?
+#else
+  char buf[0x240];
+#endif  // FORKLIB_WINDOWS_11
   memset(buf, 0, sizeof(buf));
   // if (!NT_SUCCESS(CsrClientConnectToServer(L"\\Sessions\\" CSRSS_SESSIONID
-  // L"\\Windows", 3, buf, 0x240, &trash)))
-  if (!NT_SUCCESS(
-          CsrClientConnectToServer(ObjectDirectory, 3, buf, 0x240, &trash))) {
+  // L"\\Windows", 3, buf, 0x240, &ServerToServerCall)))
+  if (!NT_SUCCESS(CsrClientConnectToServer(ObjectDirectory, 3, buf, sizeof(buf),
+                                           &ServerToServerCall))) {
     LOG("FORKLIB: CsrClientConnectToServer failed!\n");
     return FALSE;
   }
